f58c51cd1586f8333ea1536a2c34ff2d941268c9
Adriano Avelino <adriano-st777@hotmail.com>
2019-02-18 23:08:43 -0300

    Adds test user is the owner of campaign with success

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe CampaignsController, type: :controller do
      end
    end
 
+   describe 'GET #show' do
+     context 'campaign exists' do
+       context 'User is the owner of campaign' do
+         it 'Returns success' do
+           request.env["devise.mapping"] = Devise.mappings[:user]
+           current_user = FactoryBot.create(:user)
+           campaign = create(:campaign, user: current_user)
+           sign_in current_user
+           get :show, params: {id: campaign.id}
+           expect(response).to have_http_status(:success)
+         end
+       end
+     end
+   end
+
 end

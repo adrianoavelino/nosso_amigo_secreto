b7dc7312a23144921ccbac456779e2542fad62ef
Adriano Avelino <adriano-st777@hotmail.com>
2019-02-18 22:30:23 -0300

    Adds test GET#index from CampaignController

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -2,46 +2,16 @@ require 'rails_helper'
 
 RSpec.describe CampaignsController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #raffle" do
-    it "returns http success" do
-      get :raffle
-      expect(response).to have_http_status(:success)
-    end
-  end
+   describe "GET #index" do
+     it "returns http success" do
+       request.env["devise.mapping"] = Devise.mappings[:user]
+       current_user = FactoryBot.create(:user)
+       sign_in current_user
+       get :index
+       expect(response).to have_http_status(:success)
+     end
+   end
 
 end

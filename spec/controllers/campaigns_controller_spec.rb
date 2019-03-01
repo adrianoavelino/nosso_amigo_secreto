3288ea4d641c920a4646221fbba52df8d5a158eb
Adriano Avelino <adriano-st777@hotmail.com>
2019-03-01 19:48:33 -0300

    Adds Hooks and refactory CampaignsController

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -4,88 +4,68 @@ RSpec.describe CampaignsController, type: :controller do
 
   include Devise::Test::ControllerHelpers
 
-   describe "GET #index" do
-     it "returns http success" do
-       request.env["devise.mapping"] = Devise.mappings[:user]
-       current_user = FactoryBot.create(:user)
-       sign_in current_user
-       get :index
-       expect(response).to have_http_status(:success)
-     end
+  before(:each) do
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
+  end
+
+  describe "GET #index" do
+   it "returns http success" do
+     get :index
+     expect(response).to have_http_status(:success)
    end
+  end
 
-   describe 'GET #show' do
-     context 'campaign exists' do
-       context 'User is the owner of campaign' do
-         it 'Returns success' do
-           request.env["devise.mapping"] = Devise.mappings[:user]
-           current_user = FactoryBot.create(:user)
-           campaign = create(:campaign, user: current_user)
-           sign_in current_user
-           get :show, params: {id: campaign.id}
-           expect(response).to have_http_status(:success)
-         end
+  describe 'GET #show' do
+    context 'campaign exists' do
+      context 'User is the owner of campaign' do
+       it 'Returns success' do
+         campaign = create(:campaign, user: @current_user)
+         get :show, params: {id: campaign.id}
+         expect(response).to have_http_status(:success)
        end
+      end
 
-       context 'User is not owner of campaign' do
-         it 'Redirects to root' do
-           request.env["devise.mapping"] = Devise.mappings[:user]
-           current_user = FactoryBot.create(:user)
-           sign_in current_user
-           campaign = create(:campaign)
-           get(:show, params: {id:campaign.id})
-           expect(response).to redirect_to('/')
-         end
+      context 'User is not owner of campaign' do
+       it 'Redirects to root' do
+         campaign = create(:campaign)
+         get(:show, params: {id:campaign.id})
+         expect(response).to redirect_to('/')
        end
+      end
+    end
 
-       context 'campaign don\'t exists' do
-         it 'redirects to root' do
-           request.env["devise.mapping"] = Devise.mappings[:user]
-           current_user = FactoryBot.create(:user)
+    context 'campaign don\'t exists' do
+      it 'redirects to root' do
+       get :show, params: {id: 0}
+       expect(response).to redirect_to('/')
+      end
+    end
+  end
 
-           sign_in current_user
-           get :show, params: {id: 0}
-           expect(response).to redirect_to('/')
-         end
-       end
+  describe 'POST #create' do
+    before(:each) do
+     @campaign_attributes = attributes_for(:campaign, user: @current_user)
+     post :create, params: {campaign: @campaign_attributes}
+    end
 
-       describe 'POST #create' do
-         it 'Redirect to new campaign' do
-           request.env["devise.mapping"] = Devise.mappings[:user]
-           current_user = FactoryBot.create(:user)
-           sign_in current_user
-           campaign_attributes = attributes_for(:campaign, user: current_user)
-           post :create, params: {campaign: campaign_attributes}
+    it 'Redirect to new campaign' do
+     expect(response).to have_http_status(302)
+     expect(response).to redirect_to("/campaigns/#{Campaign.last.id}")
+    end
 
-           expect(response).to have_http_status(302)
-           expect(response).to redirect_to("/campaigns/#{Campaign.last.id}")
-         end
-       end
+    it "Create campaign with right attributes" do
+     expect(Campaign.last.user).to eql(@current_user)
+     expect(Campaign.last.title).to eql(@campaign_attributes[:title])
+     expect(Campaign.last.description).to eql(@campaign_attributes[:description])
+     expect(Campaign.last.status).to eql(@campaign_attributes[:status])
+    end
 
-       it "Create campaign with right attributes" do
-         request.env["devise.mapping"] = Devise.mappings[:user]
-         current_user = FactoryBot.create(:user)
-         sign_in current_user
-         campaign_attributes = attributes_for(:campaign, user: current_user)
-         post :create, params: {campaign: campaign_attributes}
-
-         expect(Campaign.last.user).to eql(current_user)
-         expect(Campaign.last.title).to eql(campaign_attributes[:title])
-         expect(Campaign.last.description).to eql(campaign_attributes[:description])
-         expect(Campaign.last.status).to eql(campaign_attributes[:status])
-       end
-
-       it "Create campaign with owner associated as a member" do
-         request.env["devise.mapping"] = Devise.mappings[:user]
-         current_user = FactoryBot.create(:user)
-         sign_in current_user
-         campaign_attributes = attributes_for(:campaign, user: current_user)
-         post :create, params: {campaign: campaign_attributes}
-
-         expect(Campaign.last.members.last.name).to eql(current_user.name)
-         expect(Campaign.last.members.last.email).to eql(current_user.email)
-       end
-     end
-   end
+    it "Create campaign with owner associated as a member" do
+     expect(Campaign.last.members.last.name).to eql(@current_user.name)
+     expect(Campaign.last.members.last.email).to eql(@current_user.email)
+    end
+  end
 
 end

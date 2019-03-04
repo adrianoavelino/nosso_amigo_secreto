930c9e5d16d9f7141ae1f1ee43f76fa70bf83c04
Adriano Avelino <adriano-st777@hotmail.com>
2019-03-04 11:25:41 -0300

    Adds tests to Mailer

--- spec/factories/member.rb
+++ spec/factories/member.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
  factory :member do
    name         { FFaker::Lorem.word }
    email        { FFaker::Internet.email }
+   token        { FFaker::Lorem.characters }
    campaign
  end
 end

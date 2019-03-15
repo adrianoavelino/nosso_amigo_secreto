26066a5baee623af09fcc8d033709e255bf09f23
Adriano Avelino <adriano-st777@hotmail.com>
2019-03-14 21:17:37 -0300

    adds index view to campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,10 +7,14 @@ class Campaign < ApplicationRecord
   enum status: [:pending, :finished]
   validates :title, :description, :user, :status, presence: true
 
+  def count_opened
+    self.members.where(open: true).count
+  end
+
   private
 
   def set_status
-    self.status
+    self.status = :pending
   end
 
   def set_member

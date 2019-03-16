550b4ec5581ff53adb9c3fb26dcaaa537e5d899b
Adriano Avelino <adriano-st777@hotmail.com>
2019-03-16 14:09:29 -0300

    fix test to mailer

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe CampaignMailer, type: :mailer do
     end
 
     it 'body have member link o set open' do
-      expect(@mail.body.encoded).to match("/members/#{@member.token}/opened")
+      expect(@mail.body.encoded).to match(/members\/[\s\w=]*\/opened/)
     end
   end
 
